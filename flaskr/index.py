6568b0d86ffb2b8b6faa29c222c5f539f9dd228a
Ming Cao <cm9999@qq.com>
2020-10-13 11:26:58 +0800

    init

---
+++ flaskr/index.py
@@ -0,0 +1,12 @@
+from flask import (
+    Blueprint, flash, g, redirect, render_template, request, url_for
+)
+
+bp = Blueprint('index', __name__)
+
+@bp.route('/index', methods=('GET', 'POST'))
+def create():
+    import time
+    time_tuple = time.localtime(time.time())
+    time_dict = {"year":time_tuple.tm_year, "month": time_tuple.tm_mon, "mday": time_tuple.tm_mday}
+    return render_template('index/index.html', time_dict = time_dict)
